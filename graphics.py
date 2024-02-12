b99d15e4e3da794070f69c6b9b22843f1432c1a3
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-02-12 21:05:42 +0000

    Created point and line classes.

--- graphics.py
+++ graphics.py
@@ -28,6 +28,23 @@ class Window:
         self.__running = False
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fil_color="black"):
+        canvas.create_line(self.p1.)
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from graphics import Window
 
+# Create a window and wait for it to close.
 def main():
-    win = Window(800, 600)
+    win = Window(400, 300)
     win.wait_for_close()
 
 main()
